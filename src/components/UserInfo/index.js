92d4ab72d62e566b99b59b4709f833aa11381540
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-21 08:11:14 +0000

    user details added

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,1 +1,15 @@
-// Write your JS code here
+import './index.css'
+
+const UserDetails = () => (
+  <div className="user-details-card">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="user-profile"
+    />
+    <h2 className="user-name">Wade Warren</h2>
+    <p className="user-profession">Software Developer at UK</p>
+  </div>
+)
+
+export default UserDetails
