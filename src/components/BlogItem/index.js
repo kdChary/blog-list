0122b86fa3577d8e8bfacc19c456fe91916db6f1
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-21 08:20:03 +0000

    blog-item added

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,1 +1,17 @@
-// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {title, description, publishedDate} = props
+
+  return (
+    <li className="blog-item">
+      <div className="blog-item-heder">
+        <p className="post-title">{title}</p>
+        <p className="post-time">{publishedDate}</p>
+      </div>
+      <p className="post-description">{description}</p>
+    </li>
+  )
+}
+
+export default BlogItem
